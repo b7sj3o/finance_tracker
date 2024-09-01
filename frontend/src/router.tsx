44251398531f81063e6ec6b957fc1782c90e8130
Vano0928 <vanyapro57@gmail.com>
2024-09-01 18:38:12 +0300

    + styles and structure

---
+++ frontend/src/router.tsx
@@ -0,0 +1,18 @@
+// import { LoginPage, RegisterPage } from './pages';
+import { createBrowserRouter } from "react-router-dom"
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!
+    </div>,
+  },
+//   {
+//     path: "/login",
+//     element: <LoginPage />
+//   },
+//   {
+//     path: '/register',
+//     element: <RegisterPage />
+//   }
+]);
