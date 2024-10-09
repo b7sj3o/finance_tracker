4b53448d46bdb71ea196622639a6d7e05601bbc3
Vitalik Fedytnyk <vitalikfedytnyk@gmail.com>
2024-10-09 14:33:40 +0300

    d

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: 'tracker-frontend-4460ec945ddd.herokuapp.com/',
-    port: 4200
-  },
   plugins: [react()],
 })
